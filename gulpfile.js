312e7b3447ab57ba3f246c8dcb7a509b7edf3a33
vishwajeet-8 <109028770+vishwajeet-8@users.noreply.github.com>
2024-06-03 19:48:54 +0530

    style card, text and background

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("kaal/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("kalki/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 function watchTask() {
-  watch(["kaal/**/*.scss"], buildStyles);
+  watch(["kalki/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
